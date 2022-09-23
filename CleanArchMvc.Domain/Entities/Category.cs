8d9b006a8faa71d11780b0cc7253363d822435f1
Marcos Raél <marcosasrael@gmail.com>
2022-09-22 22:07:48 -0300

    feat: Enriquecendo o modelo de dominio criado - I

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -4,16 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CleanArchMvc.Domain.Validation;
+
 namespace CleanArchMvc.Domain.Entities
 {
-  public class Category
+  public sealed class Category
   {
-    public int Id { get; set; }
+    public int Id { get; private set; }
 
-    public int Name { get; set; }
+    public string Name { get; private set; }
+
+    public Category(string name)
+    {
+      validateDomain(name);
+    }
+
+    public Category(int id, string name)
+    {
+      DomainExceptionValid.When(id < 0, 
+          "Invalid Id value");
+
+      Id = id;
+      Name = name;
+    }
+
+    private void validateDomain(string name)
+    {
+
+      DomainExceptionValid.When(string.IsNullOrEmpty(name), 
+          "invalid name. Name is required");
+
+      DomainExceptionValid.When(name.Length < 3,
+          "Invalid name, too short, minimum 3 charecters");
+
+      Name = name;
+
+    }
 
     public ICollection<Product> Products { get; set; }
-
-
   }
 }
---
+++ CleanArchMvc.Domain/Validation/DomainExceptionValid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Validation
+{
+  public class DomainExceptionValid : Exception
+  {
+
+    public DomainExceptionValid(string error) : base(error){}
+
+    public static void When(bool hasError, string error)
+    {
+      if (hasError)
+        throw new DomainExceptionValid(error);
+    }
+
+  }
+}
