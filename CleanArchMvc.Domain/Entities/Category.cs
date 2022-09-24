8ab3490bd72fea898f52292b070ec3393ba2aa67
Marcos Raél <marcosasrael@gmail.com>
2022-09-23 22:07:19 -0300

    chore: Implementando os testes de unidade para o projeto Domain

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+  public interface IProductRepository
+  {
+    Task<IEnumerable<Category>> GetProductsAsync();
+    Task<Category> GetByIdAsync(int? id);
+
+    Task<Product> GetProductCategoryAsync(int? id);
+
+    Task<Category> Create(Category category);
+    Task<Category> Update(Category category);
+    Task<Category> Remove(Category category);
+  }
+}
